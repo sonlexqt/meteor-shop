8ef5f7ac8f0f372b34bb2733bf606818023b48cd
Son Le <sonlexqt@gmail.com>
2016-07-07 13:47:50 +0700

    Add 'isAdmin' flag to each user

--- server/publications.js
+++ server/publications.js
@@ -10,10 +10,27 @@ Meteor.publish("products-by-vendor", function(slug){
   return Products.find({"vendor.slug" : slug})
 });
 
+/*
+db.products.find({
+	"vendor.slug": {$gte: ""}
+});
+*/
+
 Meteor.publish("products-by-sku", function(sku){
   return Products.find({sku : sku});
 });
 
 Meteor.publish("cart", function(key){
   return Carts.find({userKey : key});
+});
+
+Meteor.publish(null, function() {
+  return Meteor.users.find({_id: this.userId}, {fields: {
+  		_id: 1,
+  		emails: 1,
+  		profile: 1,
+  		username: 1,
+  		isAdmin: 1
+  	}
+  });
 });
