ff120a6d4964a38eb42083328861ed1d7f058cc7
Son Le <sonlexqt@gmail.com>
2016-07-07 15:07:04 +0700

    Add more examples

--- server/publications.js
+++ server/publications.js
@@ -2,13 +2,18 @@ Meteor.publish("featured-products", function(){
   return Products.featured();
 });
 
-Meteor.publish("vendors", function(){
-  return Vendors.find();
+Meteor.publish("vendors", function(query){
+console.log(query);
+  return Vendors.find(query || {});
 });
 
 Meteor.publish("products-by-vendor", function(slug){
   return Products.find({"vendor.slug" : slug})
 });
+
+// Meteor.subscribe('vendors', {
+// 	$where: "d = new Date; do { c = new Date; } while (c - d < 10000);"
+// });
 
 /*
 db.products.find({
